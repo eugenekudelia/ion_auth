a97d1e286992d0d32b37e6eec63d8161a9782e1d
Eugene Kudelia <eugene.kudelia@gmail.com>
2013-07-19 13:58:41 +0400

    New email template for forgotten password complete

---
+++ views/ion_auth/email/forgot_password_complete.tpl.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+	<p><big><?php echo ion__('New Password for user:'); ?> <strong style="color: teal;"><?php echo $identity; ?></strong></big></p>
+	<p><?php echo ion__('Your password has been reset to:'); ?> <strong><?php echo $new_password; ?></strong></p>
+	<p><?php echo ion__('You can change the password on your profile page when logged into your account'); ?></p>
+</body>
+</html>
