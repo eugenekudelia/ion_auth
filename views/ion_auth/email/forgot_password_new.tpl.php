8cbbf1368f267decc2b0adbd434383577d463cb0
Eugene Kudelia <eugene.kudelia@gmail.com>
2013-08-11 00:05:30 +0400

    Email views: HTML::anchor() args updated

--- views/ion_auth/email/forgot_password_new.tpl.php
+++ views/ion_auth/email/forgot_password_new.tpl.php
@@ -3,7 +3,7 @@
 	<p><?php echo ion__('Password for user', NULL, 'email').': <strong>'.$identity.'</strong>'; ?></p>
 	<p><?php echo ion__('Your forgotten password has been reset.', NULL, 'email'); ?></p>
 	<p><?php echo ion__('New password').': <strong>'.$password.'</strong>'; ?></p>
-	<p><?php echo HTML::anchor('login', ion__('Login page', NULL, 'email'), NULL, 'http'); ?></p>
+	<p><?php echo HTML::anchor('login', ion__('Login page', NULL, 'email')); ?></p>
 	<p><?php echo ion__('You can change the password on your profile page when logged in', NULL, 'email'); ?></p>
 </body>
 </html>
